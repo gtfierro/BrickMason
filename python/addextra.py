7947d60feeda2675b380b7f2e54a424d5b8ba186
Gabe Fierro <gtfierro225@gmail.com>
2018-04-14 10:16:00 -0700

    add more classes

--- python/addextra.py
+++ python/addextra.py
@@ -15,7 +15,13 @@ g.bind('bf', BRICKFRAME)
 g.add((BRICK.Green_Button_Meter, RDF.type, OWL.Class))
 g.add((BRICK.Green_Button_Meter, RDFS.subClassOf, BRICK.Building_Electric_Meter))
 
+g.add((BRICK.Building_Electric_Meter, RDF.type, OWL.Class))
+g.add((BRICK.Building_Electric_Meter, RDFS.subClassOf, BRICK.Electric_Meter))
+
 g.add((BRICK.RTU, RDFS.subClassOf, BRICK.AHU))
 g.add((BRICK.RTU, RDF.type, OWL.Class))
+
+g.add((BRICK.Illumination_Sensor, RDFS.subClassOf, BRICK.Sensor))
+g.add((BRICK.Illumination_Sensor, RDF.type, OWL.Class))
 
 g.serialize('extra_classes.ttl',format='turtle')
