767ed7c2cfc53ba8ef0c97d51d5d49cd0ec13a39
Gabe Fierro <gtfierro225@gmail.com>
2018-04-18 11:09:18 -0700

    add ttlfile driver

---
+++ python/mason/driver/ttlfile.py
@@ -0,0 +1,16 @@
+from rdflib import Graph, Namespace, URIRef, Literal
+from xbos.services.pundat import DataClient
+import coloredlogs, logging
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', fmt='%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s')
+
+RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
+BRICK = Namespace('https://brickschema.org/schema/1.0.1/Brick#')
+BRICKFRAME = Namespace('https://brickschema.org/schema/1.0.1/BrickFrame#')
+BF = BRICKFRAME
+OWL = Namespace('http://www.w3.org/2002/07/owl#')
+
+class Generator(object):
+    def __init__(self, G, cfg):
+        G.load(cfg['ttlfile'], format='turtle')
