b0ec14e1487b2557f02e93325038b367ae4dd0fb
Gabe Fierro <gtfierro225@gmail.com>
2018-04-18 10:39:23 -0700

    remove old files

--- revit/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo $1
-echo $2
-tr < "$1" -d '\000' | tail -n +1 > "$2"
--- revit/convert.sh
+++
@@ -1,5 +0,0 @@
-for file in xbos-dr-revit/*.txt; do
-    python convert.py $file
-done
-mkdir xbos-dr-brick
-mv xbos-dr-revit/*.ttl  xbos-dr-brick
