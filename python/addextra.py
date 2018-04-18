58ec36a5ad65f9ee6a9f96e07f61b24daf35b192
Gabe Fierro <gtfierro225@gmail.com>
2018-04-18 11:20:41 -0700

    add site properties and site class

--- python/addextra.py
+++ python/addextra.py
@@ -24,4 +24,29 @@ g.add((BRICK.RTU, RDF.type, OWL.Class))
 g.add((BRICK.Illumination_Sensor, RDFS.subClassOf, BRICK.Sensor))
 g.add((BRICK.Illumination_Sensor, RDF.type, OWL.Class))
 
+g.add((BRICK.Site, RDFS.subClassOf, BRICK.Location))
+g.add((BRICK.Site, RDF.type, OWL.Class))
+
+# add properties for sites
+g.add((BRICKFRAME.HumanName, RDF.type, OWL.ObjectProperty))
+g.add((BRICKFRAME.HumanName, RDFS.range, BRICKFRAME.Label))
+
+g.add((BRICKFRAME.ZipCode, RDF.type, OWL.ObjectProperty))
+g.add((BRICKFRAME.ZipCode, RDFS.range, BRICKFRAME.Label))
+
+g.add((BRICKFRAME.Timezone, RDF.type, OWL.ObjectProperty))
+g.add((BRICKFRAME.Timezone, RDFS.range, BRICKFRAME.Label))
+
+g.add((BRICKFRAME.AreaSquareFeet, RDF.type, OWL.ObjectProperty))
+g.add((BRICKFRAME.AreaSquareFeet, RDFS.range, BRICKFRAME.Label))
+
+g.add((BRICKFRAME.AreaSquareMeters, RDF.type, OWL.ObjectProperty))
+g.add((BRICKFRAME.AreaSquareMeters, RDFS.range, BRICKFRAME.Label))
+
+g.add((BRICKFRAME.NumFloors, RDF.type, OWL.ObjectProperty))
+g.add((BRICKFRAME.NumFloors, RDFS.range, BRICKFRAME.Label))
+
+g.add((BRICKFRAME.PrimaryFunction, RDF.type, OWL.ObjectProperty))
+g.add((BRICKFRAME.PrimaryFunction, RDFS.range, BRICKFRAME.Label))
+
 g.serialize('extra_classes.ttl',format='turtle')
